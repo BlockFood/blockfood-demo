fb0323bf3a1337be6e590d3d8abd613c7c762d50
Pierre ODIN <pierre.odin@hotmail.fr>
2018-03-25 19:49:11 +0200

    Display breadcrumb

--- src/front/app/demoController/Steps.ts
+++ src/front/app/demoController/Steps.ts
@@ -13,3 +13,18 @@ export const enum STEPS {
 }
 
 export const FIRST_STEP_WITH_AN_ORDER = STEPS.RESTAURANT_ACCEPT_ORDER
+
+export const getStepLabel = (step: STEPS) => {
+    return {
+        [STEPS.DEMO_NOT_STARTED]: 'As a customer, choose a sector by typing an address.',
+        [STEPS.CUSTOMER_SET_ADDRESS]: 'As a customer, choose a restaurant.',
+        [STEPS.CUSTOMER_CHOOSE_RESTAURANT]: 'As a customer, choose your order in the selected restaurant and then, validate your order.',
+        [STEPS.CUSTOMER_CREATE_ORDER]: 'As a customer, proceed to the payment of your order.',
+        [STEPS.CUSTOMER_DO_PAYMENT]: 'As a restaurant, select a waiting order and accept it.',
+        [STEPS.RESTAURANT_ACCEPT_ORDER]: 'As a restaurant, notify that the order is now ready to be delivered.',
+        [STEPS.RESTAURANT_NOTIFY_ORDER_READY]: 'As a courier, select a waiting order and accept it.',
+        [STEPS.COURIER_ACCEPT_ORDER]: 'As a courier, notify that the order is now picked and on its way to be delivered.',
+        [STEPS.COURIER_NOTIFY_ORDER_PICKED]: 'As a courier, notify that the order is now delivered.',
+        [STEPS.COURIER_NOTIFY_ORDER_DELIVERED]: 'As a customer, a restaurant or a courier, keep do whatever you want!'
+    }[step] || null
+}
--- src/front/app/views/navigator/Navigator.tsx
+++ src/front/app/views/navigator/Navigator.tsx
@@ -2,7 +2,9 @@ import * as React from 'react'
 import {connect} from 'react-redux'
 import {withRouter} from 'react-router-dom'
 import {IState} from '../../state/InitialState'
+import {CUSTOMER_PREFIX, RESTAURANT_PREFIX, COURIER_PREFIX} from '../Routes'
 import {getHelpMessageContent} from '../../demoController/HelpMessages'
+import {STEPS, getStepLabel} from '../../demoController/Steps'
 import withDemoController from '../../demoController/WithDemoController'
 import Modal from '../../components/modal/Modal'
 import {closeHelpMessage} from '../../state/Actions'
@@ -13,6 +15,13 @@ class Navigator extends React.Component<any, any> {
     constructor(props: any) {
         super(props)
 
+        const {step} = this.props
+
+        this.state = {
+            step,
+            stepLabel: getStepLabel(step)
+        }
+
         this.onHelpMessageClose = this.onHelpMessageClose.bind(this)
     }
 
@@ -20,18 +29,56 @@ class Navigator extends React.Component<any, any> {
         this.props.dispatch(closeHelpMessage())
     }
 
+    componentWillReceiveProps(nextProps: any) {
+        if (nextProps.step !== this.props.step && nextProps.step !== STEPS.DEMO_NOT_STARTED) {
+            this.setState({step: nextProps.step, stepLabel: getStepLabel(nextProps.step)})
+        }
+    }
+
     render() {
         const {visible, demoController, helpMessage} = this.props
+        const {step, stepLabel} = this.state
+
+        const getStep = (minStep: STEPS, icon: string | null = null) => {
+            const isCompleted = step >= minStep
+
+            return (
+                <div className={`${icon ? `icon ${icon}` : 'step'}${isCompleted ? ' completed' : ''}`}>
+                    {!icon ? minStep : null}
+                </div>
+            )
+        }
 
         return (
             <footer id="bf-demo-navigator" className={visible ? 'visible' : ''}>
-                <button className="restart" onClick={demoController.restart}><i className="fas fa-undo-alt"/>Restart</button>
+                <button className="restart" onClick={demoController.restart}>
+                    <i className="fas fa-undo-alt"/>Restart
+                </button>
+                <div className="progress">
+                    <div className="step-label">
+                        <div><span>{step + 1}.</span> {stepLabel}</div>
+                    </div>
+                    <div className="breadcrumb">
+                        {getStep(STEPS.CUSTOMER_SET_ADDRESS, CUSTOMER_PREFIX)}
+                        {getStep(STEPS.CUSTOMER_SET_ADDRESS)}
+                        {getStep(STEPS.CUSTOMER_CHOOSE_RESTAURANT)}
+                        {getStep(STEPS.CUSTOMER_CREATE_ORDER)}
+                        {getStep(STEPS.CUSTOMER_DO_PAYMENT)}
+                        {getStep(STEPS.RESTAURANT_ACCEPT_ORDER, RESTAURANT_PREFIX)}
+                        {getStep(STEPS.RESTAURANT_ACCEPT_ORDER)}
+                        {getStep(STEPS.RESTAURANT_NOTIFY_ORDER_READY)}
+                        {getStep(STEPS.COURIER_ACCEPT_ORDER, COURIER_PREFIX)}
+                        {getStep(STEPS.COURIER_ACCEPT_ORDER)}
+                        {getStep(STEPS.COURIER_NOTIFY_ORDER_PICKED)}
+                        {getStep(STEPS.COURIER_NOTIFY_ORDER_DELIVERED)}
+                    </div>
+                </div>
+                <a className="logo" href="http://blockfood.io" target="_blank" rel="noopener noreferrer"/>
                 {helpMessage && (
                     <Modal onImmediateClose={helpMessage.onClose} onClose={this.onHelpMessageClose}>
                         {getHelpMessageContent(helpMessage.id)}
                     </Modal>
                 )}
-                <a className="logo" href="http://blockfood.io" target="_blank" rel="noopener noreferrer"/>
             </footer>
         )
     }
@@ -39,6 +86,7 @@ class Navigator extends React.Component<any, any> {
 
 const mapStateToProps = (state: IState) => {
     return {
+        step: state.step,
         helpMessage: state.helpMessage
     }
 }
