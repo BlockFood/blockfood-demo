1ce1d5ae47b435fe800a992cdf6a58864cc26c07
Conrad <conrad@blockfood.io>
2018-03-28 12:12:35 +0200

    linear test

--- src/dapp/test/BlockFoodDemo.test.js
+++ src/dapp/test/BlockFoodDemo.test.js
@@ -26,7 +26,7 @@ contract('BlockFoodDemo', function (accounts) {
     const ownerAccount = accounts[2]
     const notOwnerAccount = accounts[1]
 
-    it('should properly set the values from the constructor', async () => {
+    it('should be deployed', async () => {
         const instance = await BlockFoodDemo.deployed()
     })
 
@@ -36,6 +36,28 @@ contract('BlockFoodDemo', function (accounts) {
         ))
     }
 
+    const customer = accounts[0]
+    const restaurant = accounts[1]
+    const courier = accounts[2]
+    const orderId = 1337
 
+    it('should sustain the whole scenario', async () => {
+        const instance = await BlockFoodDemo.deployed()
+
+        await instance.newDemo(orderId, { from: customer })
+
+        await instance.submit(orderId, restaurant, web3.toWei(0.2, 'ether'), web3.toWei(0.1, 'ether'), {
+            from: customer,
+            value: web3.toWei(0.3, 'ether')
+        })
+
+        await instance.accept(orderId, { from: restaurant })
+        await instance.ready(orderId, { from: restaurant })
+
+        await instance.picking(orderId, { from : courier })
+        await instance.delivering(orderId, { from : courier })
+
+        await instance.done(orderId, { from : courier })
+    })
 
 })
