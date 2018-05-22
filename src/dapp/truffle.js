ee2a898f4389be700868bccd891f9b3e138bc7aa
titouan <larnicol.titouan@orange.fr>
2018-05-22 17:00:15 +0200

    web3 cdn example

--- src/dapp/truffle.js
+++ src/dapp/truffle.js
@@ -19,5 +19,9 @@ module.exports = {
         port: 8545,
         network_id: "*" // Match any network id
       }
+    },
+    rpc: {
+              host: "127.0.0.1",
+              port: 8545  
     }
   };
