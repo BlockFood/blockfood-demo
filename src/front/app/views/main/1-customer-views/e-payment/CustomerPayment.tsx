ae0f9371925073e7d75d97ea3c351cbf500e654c
titouan <larnicol.titouan@orange.fr>
2018-05-22 16:02:14 +0200

    web3 with bugs

--- src/front/app/views/main/1-customer-views/e-payment/CustomerPayment.tsx
+++ src/front/app/views/main/1-customer-views/e-payment/CustomerPayment.tsx
@@ -7,13 +7,15 @@ import doWithMinTime from '../../../../utils/DoWithMinTime'
 import Api from '../../../../api/Api'
 import GoBack from '../../../../components/goBack/GoBack'
 import {setCustomerOrderInProgress, setOrders} from '../../../../state/Actions'
+import Web3 from 'web3'
 
 import './CustomerPayment.scss'
+
+
 
 class CustomerPayment extends React.Component<any, any> {
     constructor(props: any) {
         super(props)
-
         this.state = {
             loading: false,
             done: false
@@ -21,6 +23,14 @@ class CustomerPayment extends React.Component<any, any> {
 
         this.onGoBack = this.onGoBack.bind(this)
         this.onSubmit = this.onSubmit.bind(this)
+        
+        // if(typeof web3 != 'undefined'){
+        //     console.log("Using web3 detected from external source like Metamask")
+        //     this.web3 = new Web3(web3.currentProvider)
+        //  }else{
+        //     console.log("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+        //     this.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+        //  }
     }
 
     private onGoBack() {
--- src/front/index.tsx
+++ src/front/index.tsx
@@ -3,5 +3,8 @@ import * as ReactDOM from 'react-dom'
 import App from './app/App'
 import registerServiceWorker from './registerServiceWorker'
 
+
+<script type="text/javascript" src="web3.min.js"></script>
+
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement)
 registerServiceWorker()
