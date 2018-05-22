e89155ab14b9656fb35455f1e45f1d8eaa0c60ce
titouan <larnicol.titouan@orange.fr>
2018-05-22 16:04:41 +0200

    web3

--- src/dapp/truffle.js
+++ src/dapp/truffle.js
@@ -1,13 +1,23 @@
+// module.exports = {
+//     networks: {
+//         "ropsten": {
+//             network_id: 3,
+//             host: "127.0.0.1",
+//             port: 8545   // Different than the default below
+//         }
+//     },
+//     rpc: {
+//         host: "127.0.0.1",
+//         port: 8545
+//     }
+// }
+
 module.exports = {
     networks: {
-        "ropsten": {
-            network_id: 3,
-            host: "127.0.0.1",
-            port: 8545   // Different than the default below
-        }
-    },
-    rpc: {
+      development: {
         host: "127.0.0.1",
-        port: 8545
+        port: 8545,
+        network_id: "*" // Match any network id
+      }
     }
-}
+  };
