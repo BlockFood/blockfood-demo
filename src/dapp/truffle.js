18febbd6690943f2462f19ae08dab703e507cbc6
TitouanLarnicol <larnicol.titouan@orange.fr>
2018-07-05 09:38:51 +0200

    changes

--- src/dapp/truffle.js
+++ src/dapp/truffle.js
@@ -1,3 +1,5 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
     networks: {
         "ropsten": {
@@ -9,5 +11,12 @@ module.exports = {
     rpc: {
         host: "127.0.0.1",
         port: 8545
-    }
+    },
+    ropsten: {
+      provider: function() {
+        const mnemonic = "boat sick term barely expand erase loan galaxy stem shiver loan pool feed service ecology"
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/EnIOlsjoYIgrwJGjworX")
+      },
+      network_id: "3"
+    },
 }
--- src/front/app/views/start/Start.tsx
+++ src/front/app/views/start/Start.tsx
@@ -18,7 +18,7 @@ const privateKey_rest = Buffer.from('33b8f9147fb2febbea51a1fcafd917fa3aaf879e831
 const privateKey_cust = Buffer.from('66765ed03dd15aa5df8c407bbf0e85925c243359e5f9a5c51b4d30a34c213994','hex')
 web3.eth.getBalance(account_restaurant,(err:any,bal:any)=>{console.log('rest:',web3.utils.fromWei(bal,'ether'))})
 web3.eth.getBalance(account_customer,(err:any,bal:any)=>{console.log('cust:',web3.utils.fromWei(bal,'ether'))})
-
+web3.eth.accounts.privateKeyToAccount(privateKey_cust);
 web3.eth.getTransactionCount(account_customer,(err:any,txCount:any) => {
   const txObject = {
     nonce: web3.utils.toHex(txCount),
@@ -39,6 +39,29 @@ web3.eth.getTransactionCount(account_customer,(err:any,txCount:any) => {
   //     console.log('txHash:',txHash)
   // })
 })
+var contract =new web3.eth.Contract([{"constant":false,"inputs":[{"name":"orderId","type":"uint256"}],"name":"picking","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"orderId","type":"uint256"}],"name":"accept","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"orderId","type":"uint256"}],"name":"newDemo","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"orderId","type":"uint256"}],"name":"delivering","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"orderId","type":"uint256"}],"name":"done","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"orderId","type":"uint256"},{"name":"restaurant","type":"address"},{"name":"restaurantPayment","type":"uint256"},{"name":"courierPayment","type":"uint256"}],"name":"submit","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"ordersDetails","outputs":[{"name":"id","type":"uint256"},{"name":"restaurantPayment","type":"uint256"},{"name":"courierPayment","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"orders","outputs":[{"name":"id","type":"uint256"},{"name":"customer","type":"address"},{"name":"restaurant","type":"address"},{"name":"courier","type":"address"},{"name":"status","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"ordersIds","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"orderId","type":"uint256"}],"name":"ready","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"orderId","type":"uint256"},{"indexed":false,"name":"status","type":"uint8"}],"name":"OrderUpdate","type":"event"}],'0x28080026b1A3cA178024C5f17a5715CBBd4a00fC')
+const createDemo = () => {
+    web3.eth.getTransactionCount(account_customer,(err:any,txCount:any) => {
+      const dataDemo = contract.methods.newDemo(2500)
+      const txObject = {
+        nonce: web3.utils.toHex(txCount),
+        to: contract.options.address,
+        value: web3.utils.toHex(web3.utils.toWei('1','ether')),
+        gasLimit: web3.utils.toHex(21000),
+        gasPrice: web3.utils.toHex(web3.utils.toWei('10','gwei')),
+        data: dataDemo
+      }
+      console.log(txObject)
+      const tx = new Tx(txObject)
+      tx.sign(privateKey_cust)
+
+      const serializedTransaction = tx.serialize()
+      const raw = '0x' + serializedTransaction.toString('hex')
+      web3.eth.sendSignedTransaction(raw,(err:any,txHash:any)=>{
+          console.log('txHash:',txHash)
+      })
+})}
+createDemo();
 
 class Start extends React.Component<any, any> {
     constructor(props: any) {
